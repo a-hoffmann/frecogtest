fe80cddd98ba4ac9d3273301175499bdb1c31d05
a-hoffmann <hoffmaa2@ds.givaudan.com>
2017-09-13 10:58:12 +0800

    Rejigged the demo folder
    Everything should be in the right place now

---
+++ config.php
@@ -0,0 +1,54 @@
+<?php
+
+define( 'APP_ID', getenv('app_id'));
+define( 'APP_KEY', getenv('app_key'));
+define( 'API_URL', 'https://api.kairos.com');
+define( 'DEMO1_ID', '');
+define( 'CURL_API_TIMEOUT', 300);
+define( 'API_TIMEOUT', 10);
+define( 'POLL_TIMEOUT', 300);
+define( 'DEMO_SECRET_KEY', (getenv('DEMO_SECRET_KEY') ? getenv('DEMO_SECRET_KEY') : ''));
+
+return array(
+
+    "highchartsColors" => '[
+        ["#22B573"],
+        ["#1175E3"],
+        ["#FB623E"],
+        ["#DA0059"],
+        ["#752673"],
+        ["#FBB03B"]
+    ]',
+    "mediaPath" => "https://media.kairos.com/demo/emotion/",
+    "uploadFileSizeImage" => "10000000",
+    "uploadFileTypesImage" => '[
+        ["image/gif"],
+        ["image/png"],
+        ["image/jpeg"],
+        ["image/jpg"]
+    ]',
+    "uploadFileSizeVideo" => "100000000",
+    "uploadFileTypesEmotion" => '[
+        ["video/mp4"],
+        ["video/x-flv"],
+        ["video/webm"],
+        ["video/quicktime"],
+        ["video/mov"],
+        ["video/wmv"],
+        ["video/mpeg"],
+        ["video/x-msvideo"],
+        ["video/x-ms-asf"],
+        ["image/png"],
+        ["image/jpeg"],
+        ["image/jpg"],
+        ["application/octet-stream"],
+        ["image/x-ms-bmp"]
+    ]',
+    "apiTimeout" => API_TIMEOUT,
+    "pollTimeout" => POLL_TIMEOUT,
+    "demoMedia"  => array(
+        "video_1"=>DEMO1_ID
+    )
+);
+
+?>
